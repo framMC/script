ac74a7dbce0a5cbe54f0bf9ba3a6edeb29c52ab3
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2023-01-04 17:00:19 +0700

    Update WelcomeUI.lua

--- GamePlay/WelcomeUI.lua
+++ GamePlay/WelcomeUI.lua
@@ -2,41 +2,41 @@ local NotificationHolder = loadstring(game:HttpGet("https://raw.githubuserconten
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 
 Notification:Notify(
-    {Title = "VernonX", Description = "Welcome to VernonX. Thank you for using our Script."},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 7, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=12017847837", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
+    {Title = "VernonX", Description = "ยินดีต้อนรับสู่ VernonX ไม่ต้องใช้คีย์"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "https://www.roblox.com/library/12017847837/logo-VernonX", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
 )
 wait(1)
 Notification:Notify(
     {Title = "VernonX", Description = "กำหลังโหลดสคริปกรุณารอสักครู่"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 7, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=12017847837", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
 wait(1)
 Notification:Notify(
     {Title = "VernonX", Description = "กำลังปรับทรัพยากรใน Server"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 7, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=12017847837", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
 wait(1)
 Notification:Notify(
     {Title = "VernonX", Description = "ตัวนี้จะเป็นตัวทดลองการใช้งานเท้านั้นยังเป็น Beta อยู่"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 7, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=12017847837", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
 wait(1)
 Notification:Notify(
-    {Title = "VernonX", Description = "เเนะนำหากมือถือหรือคอมพิวเตอร์ของคุณช้าลงหรือค้างให้เเนะนำกดใช้งานตัวนี้เพื่อให้เครื่องของคุณลื่นขึ้น !!เเนะนำ"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 50, Type = "option"}
+    {Title = "VernonX", Description = "เเนะนำหากมือถือหรือคอมพิวเตอร์ของคุณช้าลงหรือค้างให้เเนะนำกดใช้งานตัวนี้เพื่อให้เครื่องของคุณลื่นขึ้นเเละลดพื้นผิวลงในการเล่นเกม !!เเนะนำ"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 60, Type = "option"}
 )
 wait(1)
 Notification:Notify(
     {Title = "VernonX", Description = "Creator By VernonStudio"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 16, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=12017847837", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
 wait(1)
 Notification:Notify(
     {Title = "VernonX", Description = "ขอบคุณสำหรับการสนับสนุนจากเรา"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 12, Type = "default"}
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 15, Type = "default"}
 )
