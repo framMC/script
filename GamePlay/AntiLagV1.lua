26634acf22e5246aa4cf34488a8cf7b9ec64ae16
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2023-01-04 14:43:19 +0700

    Create AntiLagV1.lua

---
+++ GamePlay/AntiLagV1.lua
@@ -0,0 +1,10 @@
+local crumbleCount = math.floor(#game.Workspace.Map.Destroyable:GetChildren() * 0.2)
+		for crumble = 0, crumbleCount, 1 do
+			local chosenBlock = game.Workspace.Map.Destroyable:GetChildren()[math.random(1, #game.Workspace.Map.Destroyable:GetChildren())]
+			chosenBlock.Color = Color3.fromRGB(255,0,0)
+			chosenBlock.Parent = game.Workspace.Map.Destroyed
+			delay(0.5, function()
+				chosenBlock.CanCollide = false
+				chosenBlock:Destroy()
+			end)
+		end
