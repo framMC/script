0b689a0ac3dabcd01389fce60fe13a739d1fd327
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2023-01-05 11:36:32 +0700

    Update WelcomeUI1.lua

--- GamePlay/WelcomeUI1.lua
+++ GamePlay/WelcomeUI1.lua
@@ -1,7 +1,7 @@
 local function callback(Text)
-	if Text == "ไม่จำเป็น" then
+	if Text == "Deny" then
 		
-	elseif Text == ("เปิดลดการ Load พื้นผิว") then
+	elseif Text == ("Accept") then
 		game:GetService("Players").loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/FPSBooster.lua"))()
 
 	end
@@ -15,7 +15,7 @@ game.StarterGui:SetCore("SendNotification",  {
 	Text = "ลดการเเหลกจากการโหลดในเกม ลดพื้นผิว เเนะนำให้ใช้งาน V2";
 	Icon = "rbxassetid://240664703";
 	Duration = 100;
-	Button1 = "เปิดลดการ Load พื้นผิว";
-	Button2 = "ไม่จำเป็น";
+	Button1 = "Accept";
+	Button2 = "Deny";
 	Callback = NotificationBindable;
 })
