368817dd11551582ca065f6518aceb161d2ed542
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-07-27 14:07:02 +0700

    Update redz.lua

--- redz.lua
+++ redz.lua
@@ -1,1 +1,18 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/script/main/GamePlay/WelcomeUI.lua", true))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/script/main/GamePlay/AntiLag.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/script/main/GamePlay/block.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/script/main/GamePlay/FpsBooster.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/script/main/hacking/Godmode.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/script/main/GamePlay/LagTest.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/script/main/GamePlay/AntiLagV1.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/script/main/GamePlay/Welcomeui1.lua", true))()
+
